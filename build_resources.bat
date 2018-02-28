7de37d1873f1f5a31790d45bc8258d2a78ab7507
ronanwow1001 <ronancaleb123@gmail.com>
2018-02-28 11:09:07 -0800

    Create build_resources.bat

---
+++ build_resources.bat
@@ -0,0 +1,14 @@
+@echo off
+load file "phase_3.mf"
+load file "phase_3.5.mf"
+load file "phase_4.mf"
+load file "phase_5.mf"
+load file "phase_5.5.mf"
+load file "phase_7.mf"
+load file "phase_8.mf"
+load file "phase_9.mf"
+load file "phase_10.mf"
+load file "phase_11.mf"
+load file "phase_12.mf"
+load file "phase_13.mf"
+run
